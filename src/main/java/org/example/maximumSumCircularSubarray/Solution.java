74386c22ab5993e450fbc7e6f57d15c29c0c5c29
miwukai <miwukai>
2025-04-24 12:49:12 +0800

    some solutions

--- src/main/java/org/example/maximumSumCircularSubarray/Solution.java
+++ src/main/java/org/example/maximumSumCircularSubarray/Solution.java
@@ -3,26 +3,26 @@ package org.example.maximumSumCircularSubarray;
 public class Solution {
     public int maxSubarraySumCircular(int[] nums) {
 
-        int result = nums[0];
+        int total = 0;
+        int maxEnding = 0;
+        int maxSum = nums[0];
+        int minEnding = 0;
+        int minSum = nums[0];
 
         for(int x = 0; x < nums.length; x++){
-            if(x == 0 || (nums[x - 1] < 0 && nums[x] >= 0)){
-
-                int maxEnding = nums[x];
-
-                for(int j = 1 + x; j < nums.length + x; j++){
-                    maxEnding = Math.max(maxEnding + nums[j % nums.length], nums[j % nums.length]);
-
-                    result = Math.max(result, maxEnding);
-                }
-            }
+            maxEnding = Math.max(maxEnding + nums[x], nums[x]);
+            maxSum = Math.max(maxEnding, maxSum);
+            minEnding = Math.min(minEnding + nums[x], nums[x]);
+            minSum = Math.min(minEnding, minSum);
+            total += nums[x];
         }
-        return result;
+
+        return maxSum > 0 ? Math.max(maxSum, total - minSum) : maxSum;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] nums = {};
-        System.out.println(solution.maxSubarraySumCircular(nums)); // Output: 3
+        System.out.println(solution.maxSubarraySumCircular(nums));
     }
 }
