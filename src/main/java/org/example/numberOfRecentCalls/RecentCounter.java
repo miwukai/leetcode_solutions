c8d6d00d196f16e8fddbf40fe6461046fead5abd
miwukai <miwukai>
2025-08-12 15:29:05 +0800

    some solutions

---
+++ src/main/java/org/example/numberOfRecentCalls/RecentCounter.java
@@ -0,0 +1,22 @@
+package org.example.numberOfRecentCalls;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class RecentCounter {
+    Queue<Integer> queue;
+
+    public RecentCounter(){
+        queue = new LinkedList<>();
+    }
+
+    public int ping(int t) {
+        queue.offer(t);
+
+        while (queue.peek() < t - 3000) {
+            queue.poll();
+        }
+
+        return queue.size();
+    }
+}
